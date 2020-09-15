3ff9ed2fbb5215d155a7da4789053e64e090a397
samuelkaranja <samuelkaranja007@gmail.com>
2020-09-15 18:35:32 +0300

    Changes

--- Photograpy/Gallery/urls.py
+++ Photograpy/Gallery/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Index),
-    path('Contact-Us/', views.Contact),
+    path('', views.Index, name="home"),
+    path('Contact-Us/', views.Contact, name="contact"),
 ]
--- Photograpy/Gallery/views.py
+++ Photograpy/Gallery/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-
+from . forms import ContactForm
 # Create your views here.
 
 def Index(request):
     return render(request, 'Gallery/index.html')
 
 def Contact(request):
-    return render(request, 'Gallery/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact')
+    else:
+
+        return render(request, 'Gallery/contact.html')
