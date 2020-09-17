93a9e4b79faf68f17c37bc25686c2adcf11bc109
samuelkaranja <samuelkaranja007@gmail.com>
2020-09-18 01:42:39 +0300

    Code change

--- Photograpy/Wedding/urls.py
+++ Photograpy/Wedding/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('wedding-photograpy/favourites/', views.Favourite, name="favourite"),
-    path('wedding-photograpy/ceremony-reception-venues/', views.Ceremony, name="ceremony"),
-    path('wedding-photograpy/engagements/', views.Engagement, name="engagement"),
+    path('wedding-photograpy/favourites/', views.Favourites, name="favourite"),
+    path('wedding-photograpy/ceremony-reception-venues/', views.Ceremonys, name="ceremony"),
+    path('wedding-photograpy/engagements/', views.Engagements, name="engagement"),
 ]
--- Photograpy/Wedding/views.py
+++ Photograpy/Wedding/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-
+from . models import *
 # Create your views here.
 
-def Favourite(request):
-    return render(request, 'Wedding/favourite.html' )
+def Favourites(request):
+    fv = Favourite.objects.all()
 
-def Ceremony(request):
-    return render(request, 'Wedding/ceremony.html')
+    return render(request, 'Wedding/favourite.html', {'fv': fv} )
 
-def Engagement(request):
-    return render(request, 'Wedding/engagements.html')
+def Ceremonys(request):
+    cr = Ceremony.objects.all()
+
+    return render(request, 'Wedding/ceremony.html', {'cr': cr})
+
+def Engagements(request):
+    eg = Engagement.objects.all()
+
+    return render(request, 'Wedding/engagements.html', {'eg': eg})
